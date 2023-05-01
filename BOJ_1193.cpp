fe009c2eb5a3dca7234d6f3803790a2923ce4af4
jhhs1009 <c6731105@gmail.com>
2023-05-01 23:33:27 +0900

    0501 cpp

--- BOJ_1193.cpp
+++ BOJ_1193.cpp
@@ -3,42 +3,17 @@
 using namespace std;
 
 int main() {
-    int num, res1, res2;
-    int cnt = 0;
+    int num;
     cin >> num;
 
-    00 11
-    01 12
-    10 21
-    11 22
-    20
-    21
-    22
+    int i = 1;
+	while (num > i) {
+		num -= i;
+		i++;
+	}
 
-
-    for (int i = 1; i < 10000000; i++)
-    {
-    
-
-        for (int j = 1; j < i+2; j++)
-        {
-            res1 = i;
-            res2 = j;
-            cnt += 1;
-
-            if (cnt ==num) {
-            break;
-            }
-
-        }
-
-        if (cnt ==num) {
-            break;
-        }
-        
-
-    }
-    
-
-    cout << res1 << "/" <<res2;
+	if (i % 2 == 1)
+		cout << i + 1 - num << '/' << num;
+	else
+		cout << num << '/' << i + 1 - num;
 }
