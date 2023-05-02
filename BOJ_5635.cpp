8eed8746b8bd3594a91db2eb127e4bb927bde31b
jhhs1009 <c6731105@gmail.com>
2023-05-02 23:57:32 +0900

    0502 cpp

--- BOJ_5635.cpp
+++ BOJ_5635.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
-#include <string.h>
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    int n, d,m,y;
-    string name;
+    int n;
+
     cin >> n;
-    for (int i=0; i<n; i++) {
-        cin >> name >> d >> m >> y;
-    }
+
+    vector<pair<pair<int, int>, pair<int, string>>> v(n);
+
+    for (int i = 0; i < n; i++)
+	{
+		cin >> v[i].second.second >> v[i].second.first >> v[i].first.second >> v[i].first.first;
+	}
+
+	sort(v.begin(), v.end());
+
+	cout << v[n-1].second.second << "\n" << v[0].second.second << "\n";
+
+	return 0;
 }
