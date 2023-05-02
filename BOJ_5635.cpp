2461492c1cbf68b13b8f3ee5130995bab65efed3
fpqaut <c6731105@gmail.com>
2023-05-02 17:58:12 +0900

    0502 cpp

---
+++ BOJ_5635.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+    int n, d,m,y;
+    string name;
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        cin >> name >> d >> m >> y;
+    }
+}
